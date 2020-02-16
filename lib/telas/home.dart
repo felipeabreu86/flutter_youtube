27cc0378fe66c12198a7d4e9fca7cb8fa8e07746
felipeabreu <felipeabreu.rj@gmail.com>
2020-02-16 16:03:21 -0300

    Se aplicado, este commit ira criar a navegacao inferior

--- lib/telas/home.dart
+++ lib/telas/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube/telas/biblioteca.dart';
+import 'package:flutter_youtube/telas/emAlta.dart';
+import 'package:flutter_youtube/telas/inicio.dart';
+import 'package:flutter_youtube/telas/inscricoes.dart';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -8,8 +12,18 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _indiceAtual = 0;
+
   @override
   Widget build(BuildContext context) {
+
+    List<Widget> telas = [
+      Inicio(),
+      EmAlta(),
+      Inscricoes(),
+      Biblioteca(),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
@@ -25,24 +39,55 @@ class _HomeState extends State<Home> {
           IconButton(
             icon: Icon(Icons.videocam),
             onPressed: () {
+              //TODO: Criar ação videocam
               print("acao videocam");
             },
           ),
           IconButton(
             icon: Icon(Icons.search),
             onPressed: () {
+              //TODO: Criar ação search
               print("acao search");
             },
           ),
           IconButton(
             icon: Icon(Icons.account_circle),
             onPressed: () {
+              //TODO: Criar ação account circle
               print("acao account_circle");
             },
           ),
         ],
       ),
-      body: Text(""),
+      body: telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indiceAtual,
+        onTap: (index) {
+          setState(() {
+            _indiceAtual = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+        items: [
+          BottomNavigationBarItem(
+            title: Text("Inicio"),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Em Alta"),
+            icon: Icon(Icons.whatshot),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Inscrições"),
+            icon: Icon(Icons.subscriptions),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Biblioteca"),
+            icon: Icon(Icons.folder),
+          ),
+        ],
+      ),
     );
   }
 }
