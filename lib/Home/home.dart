4401bf407f0884176d922f249e21c6fa34cf5809
Felipe Abreu <felipeabreu.rj@gmail.com>
2021-08-17 12:20:00 -0300

    Se aplicado, este commit irá recriar a AppBar do Youtube

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -10,6 +10,32 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.grey,
+        ),
+        backgroundColor: Colors.white,
+        title: Image.asset(
+          "assets/images/youtube.png",
+          width: 140,
+        ),
+        actions: [
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.videocam),
+          ),
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.account_circle),
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
