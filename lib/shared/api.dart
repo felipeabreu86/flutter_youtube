b1553e787183a143cfc2f973918607176a490129
Felipe Abreu <felipeabreu.rj@gmail.com>
2021-08-22 11:47:10 -0300

    Se aplicado, este commit irá atualizar o pacote http e adicionar o pacote youtube_player_flutter no projeto

--- lib/shared/api.dart
+++ lib/shared/api.dart
@@ -1,23 +1,20 @@
 import 'package:flutter_youtube/model/video.dart';
 import 'package:flutter_youtube/shared/config.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Api {
   Future<List<Video>> pesquisar(String pesquisa) async {
     List<Video> videos = [];
-    http.Response response = await http.get(
-      Config.urlBase +
-          "search"
-              "?part=snippet"
-              "&type=video"
-              "&maxResults=20"
-              "&order=date"
-              "&key=${Config.chaveYoutubeApi}"
-              "&channelId=${Config.idCanal}"
-              "&q=$pesquisa",
-    );
+    http.Response response = await http.get(Uri.parse(Config.urlBase +
+        "search"
+            "?part=snippet"
+            "&type=video"
+            "&maxResults=20"
+            "&order=date"
+            "&key=${Config.chaveYoutubeApi}"
+            "&channelId=${Config.idCanal}"
+            "&q=$pesquisa"));
     if (response.statusCode == 200) {
       Map<String, dynamic> dadosJson = json.decode(response.body);
       print(dadosJson);
